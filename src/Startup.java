674d8903d11875534663fc6cbd35c82c8a669556
mszym99 <mszymanski202@gmail.com>
2022-11-14 10:33:28 -0800

    Initial commit

---
+++ src/Startup.java
@@ -0,0 +1,11 @@
+import sbw.project.cli.CommandLineInterface;
+
+public class Startup
+{
+   public static void main(String[] args)
+   {
+      CommandLineInterface cli = new CommandLineInterface();
+      
+      cli.execute();      
+   }
+}
---
+++ src/sbw/project/cli/parser/CommandParser.java
@@ -0,0 +1,16 @@
+package sbw.project.cli.parser;
+
+import sbw.project.cli.action.ActionSet;
+
+public class CommandParser 
+{
+   public CommandParser(final ActionSet actionSet, final String text)
+   {
+      System.out.println("YOUR PARSER: CONSTRUCTOR");
+   }
+   
+   public void parse()
+   {
+      System.out.println("YOUR PARSER: PARSE");   
+   }
+}
