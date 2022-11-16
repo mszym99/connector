6bcc8ee8149cee35b1efe57e45bb33070d6258e3
mszym99 <mszymanski202@gmail.com>
2022-11-16 10:42:12 -0800

    commit

--- src/Startup.java
+++ src/Startup.java
@@ -2,10 +2,16 @@ import sbw.project.cli.CommandLineInterface;
 
 public class Startup
 {
+   public Startup(){
+
+   }
    public static void main(String[] args)
    {
       CommandLineInterface cli = new CommandLineInterface();
-      
+      /*
+      Read next line I.E "CREATE RUDDER <id> WITH LIMIT <angle> SPEED <speed> ACCELERATION <acceleration>"
+       */
+
       cli.execute();      
    }
 }
--- src/sbw/project/cli/parser/CommandParser.java
+++ src/sbw/project/cli/parser/CommandParser.java
@@ -1,17 +1,28 @@
 package sbw.project.cli.parser;
 
 import sbw.project.cli.action.ActionSet;
-
+import java.util.*;
+/*
+All doX methods in this section are in class sbw.project.cli.action.ActionCreational. Use them as factory
+methods. Teams of three choose six commands; teams of two choose three.
+ */
 public class CommandParser 
 {
+   String input;
    public CommandParser(final ActionSet actionSet, final String text)
    {
-      System.out.println("YOUR PARSER: CONSTRUCTOR");
+      Scanner scanner = new Scanner(System.in);
+      String input = scanner.nextLine();
+      System.out.println("YOUR PARSER:" + input);
+
    }
-   
+   public static String getInput(String input){
+      return input;
+   }
+
    public void parse()
    {
-      System.out.println("YOUR PARSER: PARSE");   
+      System.out.println("YOUR PARSER:" + CreationalCommands.testReturn(input));
    }
    //Testing push for matt
    //Testing push for Hannah
