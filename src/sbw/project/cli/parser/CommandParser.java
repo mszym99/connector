c65b7f86479a3e8c44ba395d08e66c4cc6cb49d5
mszym99 <mszymanski202@gmail.com>
2022-11-17 18:49:23 -0800

    Mainly worked on CreationalCommands implemented testParse() checks specific values of array given to see if it matches parameters. Then delegates the path for the remaining duties required. Completed RUDDER, ELEVATOR.. Still need 4 more per spec of project.

--- src/sbw/project/cli/parser/CommandParser.java
+++ src/sbw/project/cli/parser/CommandParser.java
@@ -1,7 +1,8 @@
 package sbw.project.cli.parser;
 
+import sbw.project.cli.action.ActionCreational;
 import sbw.project.cli.action.ActionSet;
-import java.util.*;
+
 /*
 All doX methods in this section are in class sbw.project.cli.action.ActionCreational. Use them as factory
 methods. Teams of three choose six commands; teams of two choose three.
@@ -10,11 +11,14 @@ public class CommandParser
 {
    private ActionSet actionSet;
    private String text;
+
+   public static ActionCreational actionCreational;
    public static String input;
    public CommandParser(final ActionSet actionSet, final String text)
    {
       this.actionSet = actionSet;
       this.text = text;
+      this.actionCreational = actionSet.getActionCreational(); //we will have to do this for Structural and behavioral i believe
       //parse();
    }
    public void setInput(String input){
@@ -29,7 +33,15 @@ public class CommandParser
       setInput(text);
       input = getInput();
       System.out.println("YOUR PARSER: " + input);
-      CreationalCommands.toStringArray(); //changed name of method to represent the string
+     // CreationalCommands.toStringArray(); //changed name of method to represent the string
+      //currently only works with the first instance of
+      //CREATE RUDDER R1 WITH LIMIT 100 SPEED 90 ACCELERATION 10
+      //entered into cmd prolly can fix with a for loop for all commands in the script we will write for testing:)
+      CreationalCommands create = new CreationalCommands();
+      create.testParse();
       System.out.println();
+   }
+   public static ActionCreational getAC(){
+      return actionCreational;
    }
 }
--- src/sbw/project/cli/parser/CreationalCommands.java
+++ src/sbw/project/cli/parser/CreationalCommands.java
@@ -1,28 +1,88 @@
 package sbw.project.cli.parser;
 import java.sql.SQLOutput;
 import java.util.*;
+
+import sbw.architecture.datatype.Acceleration;
+import sbw.architecture.datatype.Angle;
+import sbw.architecture.datatype.Identifier;
+import sbw.architecture.datatype.Speed;
+import sbw.project.cli.action.ActionCreational;
+import sbw.project.cli.action.ActionSet;
 import sbw.project.cli.parser.CommandParser;
+
+import javax.swing.*;
 
 public class CreationalCommands {
     //get input from CommandParser
     static String input = CommandParser.getInput();
-    public static void toStringArray(){
+    public String[] toStringArray(){
         //create the parseArray which is an array of strings from input
         String data = input;
         String parseArray[] = data.split(" ");
-
-        //for testing purposes for each string in parseArray will print with extra space resembling the array
-        for(String i : parseArray){
-            System.out.print(i + " " + " ");
-        }
+        return parseArray;
 
     }
+    public void testParse(){
+        String newArray[] = toStringArray();
+        /*for(String i : newArray){
+            System.out.print(i + " " + " ");
+        }*/
+        //test parseArray[0] to see if its CREATE regardless of case
+        if(newArray[0].equalsIgnoreCase("CREATE")){
+            //if so then check if parseArray[1] is equal to one of the following commands
+            //rudder
+            if(newArray[1].equalsIgnoreCase("RUDDER")){
+                //CREATE RUDDER R1 WITH LIMIT 100 SPEED 90 ACCELERATION 10
+                Identifier idRudder = new Identifier(newArray[2]);
+                Angle angle = new Angle(strToDouble(newArray[5]));
+                Speed speed = new Speed(strToDouble(newArray[7]));
+                Acceleration acceleration = new Acceleration(strToDouble(newArray[9]));
+                //next need to send it to doCreateRudder() with the newly converted data
+                ActionCreational newCreation = CommandParser.getAC();
+                newCreation.doCreateRudder(idRudder, angle, speed, acceleration);
+               // System.out.println("Test met");
+            }
+            //elevator
+            if(newArray[1].equalsIgnoreCase("ELEVATOR")){
+                //CREATE ELEVATOR <id> WITH LIMIT <angle> SPEED <speed> ACCELERATION <acceleration>
+                Identifier idElevator = new Identifier(newArray[2]);
+                Angle angle = new Angle(strToDouble(newArray[5]));
+                Speed speed = new Speed(strToDouble(newArray[7]));
+                Acceleration acceleration = new Acceleration(strToDouble(newArray[9]));
+                ActionCreational newCreation = CommandParser.getAC();
+                //next need to send it to doCreateElevator() with the newly converted data
+                newCreation.doCreateRudder(idElevator, angle, speed, acceleration);
+            }//We just need to implement 4 more per the specs of the project
+            //aileron
+            if(newArray[1].equalsIgnoreCase("AILERON")){
+
+            }
+            //flap
+            if(newArray[1].equalsIgnoreCase("FLAP")){
+
+            }
+            //engine
+            if(newArray[1].equalsIgnoreCase("ENGINE")){
+
+            }
+            //gear
+            if(newArray[1].equalsIgnoreCase("GEAR")){
+
+            }
+
+        }
+    }
+
     //setter if needed
     public void setInput(String input){
         this.input = input;
     }
-    //testReturn will be called for testing in CommandParser
-    public static String testReturn(String input){
-        return input;
+    //strToDouble method for specific requirements
+    public double strToDouble(String value){
+        return Double.parseDouble(value);
     }
+
+
+
+
 }
