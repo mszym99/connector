e27b6e68623a3b5a4608911b98741af17693d9bc
mszym99 <mszymanski202@gmail.com>
2022-12-01 10:18:44 -0800

    verified completion

--- src/sbw/project/cli/parser/MiscellaneousCommands.java
+++ src/sbw/project/cli/parser/MiscellaneousCommands.java
@@ -1,14 +1,25 @@
 package sbw.project.cli.parser;
 
+import sbw.architecture.datatype.Rate;
 import sbw.project.cli.action.ActionBehavioral;
 import sbw.project.cli.action.ActionMiscellaneous;
 import sbw.project.cli.action.command.misc.CommandDoExit;
+import sbw.project.cli.action.command.misc.CommandDoRunCommandFile;
+import sbw.project.cli.action.command.misc.CommandDoSetClockRate;
 
 public class MiscellaneousCommands {
     public void miscellaneousParse(String newArray[]) {
 
         if(newArray[0].equalsIgnoreCase("@CLOCK")){
-
+            //@CLOCK <rate>
+            /*
+            Sets the system clock speed to rate ticks per second.
+            This calls submitCommand() with an instance of CommandDoSetClockRate.
+             */
+            Rate rate = new Rate(Integer.parseInt(newArray[1]));
+            CommandDoSetClockRate clockCommand = new CommandDoSetClockRate(rate);
+            ActionMiscellaneous newAM = CommandParser.getAM();
+            newAM.submitCommand(clockCommand);
         }
 
         if(newArray[0].equalsIgnoreCase("@EXIT")){
@@ -18,8 +29,11 @@ public class MiscellaneousCommands {
             newMiscellaneous.submitCommand(exitCommand);
         }
 
+        //@RUN "<filename>"
         if(newArray[0].equalsIgnoreCase("@RUN")){
-
+            CommandDoRunCommandFile command = new CommandDoRunCommandFile(newArray[1]);
+            ActionMiscellaneous newAM = CommandParser.getAM();
+            newAM.submitCommand(command);
         }
 
     }
